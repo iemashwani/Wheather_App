3d1b26dc4887ae4980fe50b37823204aade8ff12
AshwaniSingh12 <as3141538@gmail.com>
2025-03-15 14:06:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./components/Weather";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Weather />
+      {/* <footer>
+        All right @Ashwani Singh 2025
+      </footer> */}
+    </div>
+  );
+};
+
+export default App;
